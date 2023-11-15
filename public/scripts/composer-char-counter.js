e1d9d7d10ecb90c9cc14bd04bf9e96e38938df5f
gisellealmario <gisellealmario@gmail.com>
2023-11-15 19:57:54 +0000

    used input event instead of keyup to trigger textarea changes

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $(".new-tweet textarea").on("keyup", function() {
+  $(".new-tweet textarea").on("input", function() {
     const inputLength = $(this).val().length; // Get the length of the current value in the textarea
     const counter = $(this).closest("form").find(".counter");
 
